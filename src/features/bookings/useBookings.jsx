7893561699311aed1c5c6e71d701cceb60b38fb0
akshayzagad <akshayzagade62@gmail.com.com>
2025-10-01 12:09:24 +0530

    Enhance booking functionality; implement filtering in useBookings, update getBookings to accept filter parameters, and integrate BookingTableOperations in Bookings page.

--- src/features/bookings/useBookings.jsx
+++ src/features/bookings/useBookings.jsx
@@ -1,11 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export default function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // filter
+  const filterValue = searchParams.get("status");
+  console.log(filterValue);
+
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // {field:"totalPrice",value:5000,method:"gte"}
+  console.log(filter);
+
   const {
     isLoading,
     data: getDataOfBookings,
     error,
-  } = useQuery({ queryKey: ["booking"], queryFn: getBookings });
+  } = useQuery({
+    queryKey: ["booking", filter],
+    queryFn: () => getBookings({ filter }),
+  });
   return { isLoading, getDataOfBookings, error };
 }
