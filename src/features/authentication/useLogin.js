e83d0ae5ae1822cfaa465e0ed501e16e5bcff7d3
akshayzagad <akshayzagade62@gmail.com.com>
2025-11-07 11:50:07 +0530

    Implement logout functionality; add useLogout hook for handling user sign-out, create Logout component with loading state, and update Header to include Logout button.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -6,12 +6,15 @@ import toast from "react-hot-toast";
 export function useLogin() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const { mutate: login, isLoading: isLogin } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
+
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"].user)
-      navigate("/dashboard");
+      queryClient.setQueriesData(["user"].user);
+      navigate("/dashboard", { replace: true });
     },
+
     onError: (err) => {
       console.log("Error", err);
       toast.error("Provided email or password is incorrect");
